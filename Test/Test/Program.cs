9e7fb8613a4c912d0946c2129980dc5313e4cdba
budInsky <budInsky@163.com>
2017-07-26 17:09:48 +0800

    addTest

---
+++ Test/Test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Protocal;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var acBuilder = Account.CreateBuilder();
+
+            acBuilder.Account_ = "1324";
+            acBuilder.Password = "456";
+            byte[] buff = acBuilder.Build().ToByteArray();
+
+            Account account = Account.ParseFrom(buff);
+            Console.WriteLine("Account = " + account.Account_);
+            Console.WriteLine("Password = " + (account.Password));
+
+            Console.ReadKey();
+        }
+    }
+}
